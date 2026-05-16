40cbb971a2afb7ce0ccb0f516c6763d322ecdc73
asaeidi83 <asaeidi83@gmail.com>
2026-05-16 11:47:03 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,9 +80,6 @@ export const metadata: Metadata = {
       'max-video-preview': -1,
     },
   },
-  verification: {
-    google: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
-  },
   manifest: '/site.webmanifest',
 };
 
